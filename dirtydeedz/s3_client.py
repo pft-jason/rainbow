48625a4d491172350dc5c499a0f45fd8930482ba
jason <panelforgetechnologies@gmail.com>
2024-08-08 21:49:07 +1200

    setting up dev env

---
+++ dirtydeedz/s3_client.py
@@ -0,0 +1,23 @@
+import boto3
+from botocore.client import Config
+from .settings import get_env_variable
+
+# Configuration
+access_key = get_env_variable('DO_ACCESS_KEY_ID')
+secret_key = get_env_variable('DO_SECRET_ACCESS_KEY')
+region = 'nyc3'  # Replace with your region
+endpoint_url = get_env_variable('DO_SPACES_ENDPOINT')  # Replace with your region's endpoint
+
+# Initialize session and client
+session = boto3.session.Session()
+client = session.client(
+    's3',
+    region_name=region,
+    endpoint_url=endpoint_url,
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    config=Config(signature_version='s3v4')  # Ensure you're using v4
+)
+
+# Verify signing method
+print(f"Signature Version: {client.meta.config.signature_version}")

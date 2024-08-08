e699e94bb716c249a332fb51422ea024e9edd78c
jason <panelforgetechnologies@gmail.com>
2024-08-09 02:10:05 +1200

    debugging

--- gallery/middleware.py
+++ gallery/middleware.py
@@ -6,10 +6,12 @@ class AgeVerificationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Exclude the age verification URL to prevent infinite redirect loop
-        if request.path != reverse('age_verification'):
+        # Exclude the age verification URL and other specific URLs to prevent infinite redirect loop
+        excluded_paths = [reverse('age_verification'), reverse('image_delete')]
+        if request.path not in excluded_paths:
             # Check if the user has verified their age
             if not request.session.get('age_verified', False):
+                logger.info(f"User has not verified age. Redirecting to age verification page from {request.path}")
                 # If not verified, redirect to the age verification page
                 return redirect('age_verification')
         
